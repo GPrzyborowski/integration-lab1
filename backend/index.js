1bccf6d2afd5a91fe809a123fa7b0ac511c11e7a
Gabriel Przyborowski <gabrielprzyborowski@outlook.com>
2026-03-16 02:38:50 +0100

    import and use sha256 hashing endpoint

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,6 @@
 import express from 'express'
+import hash from './routes/hash.routes.js'
+
 const app = express();
 
 const PORT = 3000;
@@ -10,6 +12,8 @@ app.get('/', (req, res) => {
 app.get("/api/health", (req, res) => {
   res.json({ status: "ok" });
 });
+
+app.use('/api', hash)
 
 app.listen(PORT, () => {
   console.log(`Serwer działa na porcie ${PORT}`);
